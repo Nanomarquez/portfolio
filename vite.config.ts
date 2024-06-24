a1c6f713b967a766aa5f5409f7a6b1b2e1b2eb36
Nanomarquez <nanomarqueztres@gmail.com>
2024-06-24 20:44:24 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite"
+import { remixDevTools } from "remix-development-tools"
+import {  } from ""
 
 export default defineConfig({
   server : {
@@ -8,12 +11,19 @@ export default defineConfig({
     open: true,
   },
   plugins: [
+    remixDevTools(),
     remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
+      routes(defineRoutes) {
+        return defineRoutes( (define) => {
+          define("/", "routes/_index.tsx");
+        });
+      },
+      presets: [vercelPreset()]
     }),
     tsconfigPaths(),
   ],
