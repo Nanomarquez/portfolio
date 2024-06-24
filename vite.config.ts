48d25bab60a9c8ce05f01c80645e285178382faf
Nanomarquez <nanomarqueztres@gmail.com>
2024-06-24 20:31:19 -0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server : {
+    port: 3000,
+    open: true,
+  },
   plugins: [
     remix({
       future: {
